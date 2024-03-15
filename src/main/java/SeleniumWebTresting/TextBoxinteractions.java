23aebdc85df4a197f74f2f624a4561b2fa808c83
934617 <934617@LTIN352257.cts.com>
2024-03-15 23:11:00 +0530

    Commit

--- src/main/java/SeleniumWebTresting/TextBoxinteractions.java
+++ src/main/java/SeleniumWebTresting/TextBoxinteractions.java
@@ -1,16 +1,16 @@
 package SeleniumWebTresting;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 
 public class TextBoxinteractions {
-
+	
 	public void TextBoxinteractions(WebDriver driver) {
 		System.out.println("----------Text Box interactions--------");
 		driver.findElement(By.linkText("Edit")).click();
 		driver.findElement(By.id("fullName")).sendKeys("Sujitha Balasathiya");
-		driver.findElement(By.id("join")).sendKeys("Human", Keys.TAB);
+		driver.findElement(By.id("join")).sendKeys("Human",Keys.TAB);
 		String Value = driver.findElement(By.id("getMe")).getAttribute("value");
 		System.out.println("Getme value : " + Value);
 		driver.findElement(By.id("clearMe")).clear();
